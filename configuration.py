b101b4f82c378895b1bbc0cf8bff9dc5dafb23dd
Juan Burgos <burgos.135@gmail.com>
2025-03-23 13:59:58 -0600

    First commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-6a79accf-6459-4dfc-b7e9-335199b5f0fd.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
